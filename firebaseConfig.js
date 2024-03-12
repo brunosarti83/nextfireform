c660818eb8f33798ee555650043d68cb98f51426
brunosarti83 <brunosarti.bs@gmail.com>
2024-03-12 20:22:09 -0300

    initial commit next and firebase already set

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnVV9qK2l2y4dLeYE8nEaS-NVjHAxyxSg",
+  authDomain: "next-fire-form.firebaseapp.com",
+  projectId: "next-fire-form",
+  storageBucket: "next-fire-form.appspot.com",
+  messagingSenderId: "555843677057",
+  appId: "1:555843677057:web:0695fd97e79cf97bfad680",
+  measurementId: "G-PCLCLDY07R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
