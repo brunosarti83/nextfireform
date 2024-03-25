398bdec1bb8cf8c7f18e01feb0217cbbb16ccd32
brunosarti83 <brunosarti.bs@gmail.com>
2024-03-25 20:11:21 -0300

    firestoreHAndlers: add, read, delete

--- src/firestoreHandlers.ts
+++ src/firestoreHandlers.ts
@@ -1,6 +1,6 @@
 // firestore CRUD https://www.youtube.com/watch?v=uikATllLdRc
 
-import {collection, addDoc} from 'firebase/firestore'
+import {collection, addDoc, onSnapshot, query, doc, deleteDoc} from 'firebase/firestore'
 import {db} from '../firebaseConfig'
 
 // Add item to database
@@ -10,7 +10,27 @@ export const addItem = async (stateData: {[key:string]:any}) => {
     
 
 // Read items from database
-
+export const readItems = () => {
+    const q = query(collection(db, 'items'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const itemsArr = []
+        querySnapshot.forEach((doc) => {
+            itemsArr.push({...doc.data(), id: doc.id})
+        })
+    })
+    return unsubscribe
+    // if this works as I expect use in useEffect as const unsubscribe = readItems() and return () => unsubscribe()
+}
 
 
 // Delete item from database
+export const deleteItem = async (id: string): Promise<boolean> => {
+    try {
+        const docRef = doc(db, 'items', id)
+        await deleteDoc(docRef)
+        return true
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
