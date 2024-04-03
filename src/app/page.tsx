241c498dde2d7920caead2396629f78a001cc283
brunosarti83 <brunosarti.bs@gmail.com>
2024-04-03 20:04:17 -0300

    general styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Form from "@/Components/Form/Form";
 import { formData } from "@/types";
 import { promises as fs } from 'fs';
+import Image from "next/image";
 
 export default async function Home() {
   const file = await fs.readFile(process.cwd() + '/src/formData.json', 'utf8');
   const data:formData = JSON.parse(file);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-row-reverse min-h-screen items-center justify-between p-4 bg-gray-50">
+      <div className="w-1/3"> 
+        <Image src="/buildings.jpg" alt="black and white urban picture" width={1000} height={1000}/>
+      </div>
       <Form formData={data}/>
     </main>
   );
--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -19,9 +19,19 @@ export default function Results() {
 
   return (
     <div>
-        <h1>Results</h1>
-        <DataTable columns={columns} data={items} />
-        <Button onClick={() => console.log(items)}>Log</Button>
+        <section 
+        className="flex flex-col justify-center items-center h-[350px] bg-dot-gray-500/[0.5] relative"
+        >
+            <h1 className="text-6xl font-extrabold text-gray-800 z-50">Results</h1>
+            <h3 className="text-xl font-bold text-gray-800 z-50">This are the results so far...</h3>
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,black,transparent_80%)]"></div>
+        </section>
+        <section
+        className="p-8"
+        >
+            <DataTable columns={columns} data={items} />
+        </section>
+        {/* <Button onClick={() => console.log(items)}>Log</Button> */}
     </div>
   )
 }
